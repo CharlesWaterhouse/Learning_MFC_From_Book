1b519230262d7afb96282fff319e142148c23e90
peterpeter9543 <peterpeter9543@gmail.com>
2018-12-21 17:30:12 +0800

    the first edtion is not complete program. In this case, I let is can run.

--- My_MFC_Git_Test_1221/my_mfc_git_test_class.cpp
+++ My_MFC_Git_Test_1221/my_mfc_git_test_class.cpp
@@ -3,29 +3,42 @@
 #include "resource.h"
 #include "my_mfc_git_test_class.h"
 
-//class MyFrame : public CFrameWnd 
-BEGIN_MESSAGE_MAP(MyFrame, CFrameWnd)
-	ON_COMMAND(ID_FILE_EXIT1, OnExit)
+
+//class MyDocument : public CDocument
+IMPLEMENT_DYNCREATE(MyDocument, CDocument)
+BEGIN_MESSAGE_MAP(MyDocument, CDocument)
+END_MESSAGE_MAP()
+
+//class MyView : public CView 
+IMPLEMENT_DYNCREATE(MyView, CView)
+BEGIN_MESSAGE_MAP(MyView, CView)
 	ON_WM_LBUTTONDOWN()
 	ON_WM_MOUSEMOVE()
 	ON_WM_LBUTTONUP()
 END_MESSAGE_MAP()
-afx_msg void MyFrame::OnExit() {
-	MessageBox(_T("Exit1"));
-	DestroyWindow();
+void MyView::OnDraw(CDC* p_dc) {
+	MyDocument* p_doc = (MyDocument*)GetDocument();
+	int num = p_doc->GetSize();
+	for (int i = 0; i < num; ++i) {
+		CPoint point = p_doc->GetPoint(i);
+		p_dc->SetPixel(point, RGB(255, 0, 0));
+	}
 }
-afx_msg void MyFrame::OnLButtonDown(UINT nFlags, CPoint point) {
-	SetCapture();
-}
-afx_msg void MyFrame::OnLButtonUp(UINT nFlags, CPoint point){
-	ReleaseCapture();
-}
-afx_msg void MyFrame::OnMouseMove(UINT nFlags, CPoint point) {
+afx_msg void MyView::OnLButtonDown(UINT, CPoint point) { SetCapture(); }
+afx_msg void MyView::OnLButtonUp(UINT, CPoint point) { ReleaseCapture(); }
+afx_msg void MyView::OnMouseMove(UINT, CPoint point) { 
 	if (this == GetCapture()) {
 		CClientDC aDC(this);
 		aDC.SetPixel(point, RGB(255, 0, 0));
+		MyDocument* p_doc = (MyDocument*)GetDocument();
+		p_doc->AddPoint(point);
 	}
 }
+
+//class MyFrame : public CFrameWnd 
+IMPLEMENT_DYNCREATE(MyFrame, CFrameWnd)
+BEGIN_MESSAGE_MAP(MyFrame, CFrameWnd)
+END_MESSAGE_MAP()
 
 //class MyApp : public CWinApp 
 
--- My_MFC_Git_Test_1221/my_mfc_git_test_class.h
+++ My_MFC_Git_Test_1221/my_mfc_git_test_class.h
@@ -10,43 +10,33 @@ class MyFrame;
 class MyApp;
 
 class MyDocument : public CDocument {
-public:
-	DECLARE_DYNCREATE(MyDocument)            //declares it's run-time class
+	DECLARE_DYNCREATE(MyDocument)                        //declares it's run-time class
 	DECLARE_MESSAGE_MAP()
+public:
+	void AddPoint(CPoint p) { array_cpoint.Add(p); }     // adds a point mouse has went 
+	CPoint GetPoint(int i) { return array_cpoint[i]; }   // gets a point from array_cpoint
+	int GetSize() { return array_cpoint.GetSize(); }     // get the amount of recorded points
 private:
-	CArray<CPoint, CPoint&> p_array_cpoint;
+	CArray<CPoint, CPoint&> array_cpoint;                //records the points that mouse has went through.
 };
-IMPLEMENT_DYNCREATE(MyDocument, CDocument)
+
 
 class MyView : public CView {
+	DECLARE_DYNCREATE(MyView)                            //declares it's run-time class
+	DECLARE_MESSAGE_MAP()
 public:
-	void OnDraw(CDC* p_dc) {}                // for override
-	DECLARE_DYNCREATE(MyView)                //declares it's run-time class
+	void OnDraw(CDC* p_dc);                              // for override
+	afx_msg void OnLButtonDown(UINT, CPoint point);
+	afx_msg void OnMouseMove(UINT, CPoint point);
+	afx_msg void OnLButtonUp(UINT, CPoint point);
 };
-IMPLEMENT_DYNCREATE(MyView, CView)              
+         
 
 class MyFrame : public CFrameWnd {
-private:
-	CMenu *p_f_menu;
-	
-public:
-	MyFrame() {
-		Create(NULL, _T("Hellow"));
-		p_f_menu = new CMenu;
-		p_f_menu->LoadMenu(IDR_MENU1);
-		SetMenu(p_f_menu);
-	}
-	~MyFrame() { delete p_f_menu; }
-
-	DECLARE_DYNCREATE(MyFrame)                //declares it's run-time class
+	DECLARE_DYNCREATE(MyFrame)                           //declares it's run-time class
 	DECLARE_MESSAGE_MAP();
-	afx_msg void OnExit();
-	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
-	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
-	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
-
 };
-IMPLEMENT_DYNCREATE(MyFrame, CFrameWnd)
+
 
 class MyApp : public CWinApp {
 public:
