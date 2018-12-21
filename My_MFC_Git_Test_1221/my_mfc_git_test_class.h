58b3e626f0988825d9175585dfc392dce11d06b4
peterpeter9543 <peterpeter9543@gmail.com>
2018-12-21 17:54:42 +0800

    nothing

--- My_MFC_Git_Test_1221/my_mfc_git_test_class.h
+++ My_MFC_Git_Test_1221/my_mfc_git_test_class.h
@@ -20,7 +20,6 @@ private:
 	CArray<CPoint, CPoint&> array_cpoint;                //records the points that mouse has went through.
 };
 
-
 class MyView : public CView {
 	DECLARE_DYNCREATE(MyView)                            //declares it's run-time class
 	DECLARE_MESSAGE_MAP()
@@ -30,27 +29,19 @@ public:
 	afx_msg void OnMouseMove(UINT, CPoint point);
 	afx_msg void OnLButtonUp(UINT, CPoint point);
 };
-         
 
 class MyFrame : public CFrameWnd {
 	DECLARE_DYNCREATE(MyFrame)                           //declares it's run-time class
 	DECLARE_MESSAGE_MAP();
 };
 
-
 class MyApp : public CWinApp {
 public:
 	BOOL InitInstance() {
-		
 		/*this document class is a object for integrate that includes resources, the document, view, and frame classes.*/
 		CSingleDocTemplate* p_c_single_document_template;
 		p_c_single_document_template = new CSingleDocTemplate(IDR_MENU1, RUNTIME_CLASS(MyDocument), RUNTIME_CLASS(MyFrame), RUNTIME_CLASS(MyView));
 		AddDocTemplate(p_c_single_document_template);            // we set the template object to application object(this MyApp object)
-		
-		//CFrameWnd* p_frame = new MyFrame();
-		//m_pMainWnd = p_frame;
-		//p_frame->ShowWindow(SW_SHOW);
-
 		CDocument* p_c_document;
 		p_c_document = p_c_single_document_template->CreateNewDocument();                         //create document object by template object
 		m_pMainWnd = p_c_single_document_template->CreateNewFrame(p_c_document, NULL);            //create frame object by template object
