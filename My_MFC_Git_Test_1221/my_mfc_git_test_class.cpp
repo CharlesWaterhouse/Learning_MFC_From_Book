e2da6abf8c0e17b5c1d1c7095028755adebbe3ce
peterpeter9543 <peterpeter9543@gmail.com>
2018-12-24 14:52:25 +0800

    The chapter 9 exercise program has been completed. Learning the ON_UPDATE_COMMAND_UI, but I am not understand when the function be called yet.

--- My_MFC_Git_Test_1221/my_mfc_git_test_class.cpp
+++ My_MFC_Git_Test_1221/my_mfc_git_test_class.cpp
@@ -3,144 +3,44 @@
 #include "resource.h"
 #include "my_mfc_git_test_class.h"
 
-
-////class MyDocument : public CDocument
-//IMPLEMENT_DYNCREATE(MyDocument, CDocument)
-//BEGIN_MESSAGE_MAP(MyDocument, CDocument)
-//END_MESSAGE_MAP()
-//
-////class MyView : public CView 
-//IMPLEMENT_DYNCREATE(MyView, CView)
-//BEGIN_MESSAGE_MAP(MyView, CView)
-//	ON_WM_LBUTTONDOWN()
-//	ON_WM_MOUSEMOVE()
-//	ON_WM_LBUTTONUP()
-//END_MESSAGE_MAP()
-//void MyView::OnDraw(CDC* p_dc) {
-//	MyDocument* p_doc = (MyDocument*)GetDocument();
-//	int num = p_doc->GetSize();
-//	for (int i = 0; i < num; ++i) {
-//		CPoint point = p_doc->GetPoint(i);
-//		p_dc->SetPixel(point, RGB(255, 0, 0));
-//	}
-//}
-//afx_msg void MyView::OnLButtonDown(UINT, CPoint point) { SetCapture(); }
-//afx_msg void MyView::OnLButtonUp(UINT, CPoint point) { ReleaseCapture(); }
-//afx_msg void MyView::OnMouseMove(UINT, CPoint point) { 
-//	if (this == GetCapture()) {
-//		CClientDC aDC(this);
-//		aDC.SetPixel(point, RGB(255, 0, 0));
-//		MyDocument* p_doc = (MyDocument*)GetDocument();
-//		p_doc->AddPoint(point);
-//	}
-//}
-
 //class MyFrame : public CFrameWnd 
 BEGIN_MESSAGE_MAP(MyFrame, CFrameWnd)
-	ON_COMMAND(ID_SWITCHMENU, OnSwitchMenu)
-	ON_COMMAND(ID_DYNAMICMENU_CREATEITEM, OnCreateItem)
-	ON_COMMAND(IDM_DeleteItem, OnDeleteItem)
-	ON_COMMAND(ID_RED, OnRed)
-	ON_COMMAND(ID_GREEN, OnGreen)
-	ON_COMMAND(ID_BLUE, OnBlue)
+	ON_COMMAND(ID_COLORMENU_RED, OnRed)
+	ON_COMMAND(ID_COLORMENU_GREEN, OnGreen)
+	ON_COMMAND(ID_COLORMENU_BLUE, OnBlue)
+	ON_UPDATE_COMMAND_UI(ID_COLORMENU_RED, OnUpdateRed)
+	ON_UPDATE_COMMAND_UI(ID_COLORMENU_GREEN, OnUpdateGreen)
+	ON_UPDATE_COMMAND_UI(ID_COLORMENU_BLUE, OnUpdateBlue)
 	ON_WM_LBUTTONDOWN()
-	ON_WM_MOUSEMOVE()
 	ON_WM_LBUTTONUP()
-	ON_WM_CONTEXTMENU()
+	ON_WM_MOUSEMOVE()
 END_MESSAGE_MAP()
 MyFrame::MyFrame() {
 	Create(NULL,                                   //create a standard window
-			   _T("Menu"),                             //window title
+			   _T("COMMAND_UI"),                             //window title
 				 WS_OVERLAPPEDWINDOW | WS_VISIBLE,       //window style
 				 rectDefault,                            //window size
 				 NULL,                                   //the pointer of parent window
-				 MAKEINTRESOURCE(IDR_MENU4));            //the ID of menu
-	system_menu = GetSystemMenu(FALSE);            //gets the system menu
-	system_menu->AppendMenu(MF_SEPARATOR);        //inserts the separator line
-	system_menu->AppendMenu(MF_STRING, 0, _T("Insert by me!"));          //inserts the option
+				 MAKEINTRESOURCE(IDR_MENU6));            //the ID of menu
 	color = RGB(255, 0, 0);                        //sets the point color to red
 }
-void MyFrame::SetCheck() {
-	switch (color) {
-	  case RGB(255, 0, 0): {
-		main_menu.CheckMenuItem(ID_RED, MF_BYCOMMAND | MF_CHECKED);
-		break;
-	  }
-	  case RGB(0, 255, 0): {
-		main_menu.CheckMenuItem(ID_GREEN, MF_BYCOMMAND | MF_CHECKED);
-		break;
-	  }
-	  case RGB(0, 0, 255): {
-		main_menu.CheckMenuItem(ID_BLUE, MF_BYCOMMAND | MF_CHECKED);
-		break;
-	  }
-	  default: {
-		assert(false);
-	  }
-	}
-}
-void MyFrame::SetUnCheck() {
-	switch (color) {
-		case RGB(255, 0, 0): {
-			main_menu.CheckMenuItem(ID_RED, MF_BYCOMMAND | MF_UNCHECKED);
-			break;
-		}
-		case RGB(0, 255, 0): {
-			main_menu.CheckMenuItem(ID_GREEN, MF_BYCOMMAND | MF_UNCHECKED);
-			break;
-		}
-		case RGB(0, 0, 255): {
-			main_menu.CheckMenuItem(ID_BLUE, MF_BYCOMMAND | MF_UNCHECKED);
-			break;
-		}
-		default: {
-			assert(false);
-		}
-	}
-}
-afx_msg void MyFrame::OnSwitchMenu() {
-	CMenu insert_menu;                                  //create a menu object
-	insert_menu.LoadMenu(IDR_MENU5);                    //load the menu source
-	main_menu.LoadMenu(IDR_MENU3);
-	main_menu.AppendMenu(MF_POPUP, (UINT)insert_menu.m_hMenu, _T("Color Menu"));
-	sub_menu = main_menu.GetSubMenu(1);
-	sub_menu->AppendMenuW(MF_SEPARATOR);
-	sub_menu->InsertMenu(ID_DYNAMICMENU_CREATEITEM, MF_BYCOMMAND | MF_STRING, 0, _T("Insert a Item"));
-	sub_menu->InsertMenu(ID_DYNAMICMENU_CREATEITEM, MF_BYCOMMAND | MF_POPUP, (UINT) insert_menu.m_hMenu, _T("Insert a Item"));
-	SetCheck();
-	sub_menu->InsertMenu(ID_DYNAMICMENU_CREATEITEM, MF_BYCOMMAND | MF_SEPARATOR);
-	insert_menu.Detach();
-	SetMenu(&main_menu);
-}
-afx_msg void MyFrame::OnCreateItem() {
-	main_menu.ModifyMenu(ID_DYNAMICMENU_CREATEITEM, MF_BYCOMMAND, IDM_DeleteItem, _T("Delete Item"));
-	sub_menu->AppendMenu(MF_STRING, IDM_NewItem, _T("New Item"));
-}
-afx_msg void MyFrame::OnDeleteItem() {
-	main_menu.ModifyMenu(IDM_DeleteItem, MF_BYCOMMAND, ID_DYNAMICMENU_CREATEITEM, _T("Create Item"));
-	sub_menu->DeleteMenu(IDM_NewItem, MF_BYCOMMAND);
-}
-afx_msg void MyFrame::OnContextMenu(CWnd* p_wnd, CPoint point) {
-	CMenu menu;
-	menu.LoadMenu(IDR_MENU2);
-	pop_menu = menu.GetSubMenu(0);
-	pop_menu->TrackPopupMenu(TPM_CENTERALIGN | TPM_RIGHTBUTTON, point.x, point.y, this);
-	menu.Detach();
-}
 afx_msg void MyFrame::OnRed() {
-	SetUnCheck();
 	color = RGB(255, 0, 0);
-	SetCheck();
 }
 afx_msg void MyFrame::OnGreen() {
-	SetUnCheck();
 	color = RGB(0, 255, 0);
-	SetCheck();
 }
 afx_msg void MyFrame::OnBlue() {
-	SetUnCheck();
 	color = RGB(0, 0, 255);
-	SetCheck();
+}
+afx_msg void MyFrame::OnUpdateRed(CCmdUI* a_cmd_ui) {
+	a_cmd_ui->SetCheck(color == RGB(255, 0, 0));
+}
+afx_msg void MyFrame::OnUpdateBlue(CCmdUI* a_cmd_ui) {
+	a_cmd_ui->SetCheck(color == RGB(0, 255, 0));
+}
+afx_msg void MyFrame::OnUpdateGreen(CCmdUI* a_cmd_ui) {
+	a_cmd_ui->SetCheck(color == RGB(0, 0, 255));
 }
 afx_msg void MyFrame::OnLButtonDown(UINT n_flags, CPoint point) {
 	SetCapture();
@@ -155,20 +55,7 @@ afx_msg void MyFrame::OnMouseMove(UINT n_flags, CPoint point) {
 	}
 }
 
-
 //class MyApp : public CWinApp 
-//BOOL MyApp::InitInstance() {
-//	/*this document class is a object for integrate that includes resources, the document, view, and frame classes.*/
-//	CSingleDocTemplate* p_c_single_document_template;
-//	p_c_single_document_template = new CSingleDocTemplate(IDR_MENU1, RUNTIME_CLASS(MyDocument), RUNTIME_CLASS(MyFrame), RUNTIME_CLASS(MyView));
-//	AddDocTemplate(p_c_single_document_template);                                             // we set the template object to application object(this MyApp object)
-//	CDocument* p_c_document;
-//	p_c_document = p_c_single_document_template->CreateNewDocument();                         //create document object by template object
-//	m_pMainWnd = p_c_single_document_template->CreateNewFrame(p_c_document, NULL);            //create frame object by template object
-//	p_c_single_document_template->InitialUpdateFrame((CFrameWnd*)m_pMainWnd, p_c_document);   //initializes the frame object and links it to view object
-//	m_pMainWnd->ShowWindow(SW_SHOW);
-//	return true;
-//}
 BOOL MyApp::InitInstance() {
 	CFrameWnd* p_frame = new MyFrame;
 	m_pMainWnd = p_frame;
--- My_MFC_Git_Test_1221/resource.h
+++ My_MFC_Git_Test_1221/resource.h
@@ -2,35 +2,22 @@
 // Microsoft Visual C++ 產生的 Include 檔案。
 // 由 My_MFC_Git_Test_1221.rc 使用
 //
-#define IDR_MENU1                       101
-#define IDR_MENU2                       113
-#define IDR_MENU3                       114
-#define IDR_MENU4                       118
-#define IDR_MENU5                       119
+#define IDR_MENU6                       120
 #define ID_FILE_EXIT                    40001
-#define ID_FILE_EXIT1                   40002
-#define ID_FILE_EXIT2                   40003
 #define IDR_POP                         40004
 #define ID_KJH                          40005
 #define ID_Menu                         40007
-#define ID__POPITEM1                    40009
-#define ID__POPITEM2                    40010
-#define ID_FILE_EXIT40011               40011
 #define ID_GREEN                        40015
-#define ID_RED                          40018
-#define ID_GREEN40019                   40019
-#define ID_BLUE                         40020
-#define ID_DYNAMICMENU_CREATEITEM       40021
-#define ID_SWITCHMENU                   40022
-#define IDM_DeleteItem                  40023
-#define IDM_NewItem                     40024
+#define ID_COLORMENU_RED                40022
+#define ID_COLORMENU_GREEN              40023
+#define ID_COLORMENU_BLUE               40024
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        120
-#define _APS_NEXT_COMMAND_VALUE         40022
+#define _APS_NEXT_RESOURCE_VALUE        121
+#define _APS_NEXT_COMMAND_VALUE         40026
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
